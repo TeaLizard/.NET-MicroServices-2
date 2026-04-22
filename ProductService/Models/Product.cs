2ddc403b5b216e5d8c3194a719acd4ebe210a0f7
TeaLizard <owentwilliams7@gmail.com>
2026-04-22 15:00:15 -0400

    Product model

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductService.Models;
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
