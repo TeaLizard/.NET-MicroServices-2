af5583d440605e51cb0106e1c95cca517787e71c
TeaLizard <owentwilliams7@gmail.com>
2026-04-22 15:30:34 -0400

    product program.cs

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,23 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlite("Data Source=products.db"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
-
 app.Run();
